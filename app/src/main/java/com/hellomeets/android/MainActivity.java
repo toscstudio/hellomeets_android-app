263a9454812dd93a289a15dc00fd843199d6099c
Arnav Gupta <championswimmer@gmail.com>
2016-04-10 08:08:06 +0530

    add fragment transactions

--- app/src/main/java/com/hellomeets/android/MainActivity.java
+++ app/src/main/java/com/hellomeets/android/MainActivity.java
@@ -2,19 +2,29 @@ package com.hellomeets.android;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.hellomeets.android.fragment.EventListFragment;
+import com.hellomeets.android.fragment.dummy.DummyContent;
 import com.hellomeets.android.intro.IntroActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EventListFragment.OnListFragmentInteractionListener{
 
     public static final String PREF_INTRO_SHOWN = "intro_shown";
 
@@ -48,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         drawerFrame = (FrameLayout) findViewById(R.id.left_drawer);
         navList = (ListView) findViewById(R.id.nav_list);
 
-        ArrayAdapter<String> navCategoryAdapter = new ArrayAdapter<>(this,R.layout.list_item_nav_category);
+        final ArrayAdapter<String> navCategoryAdapter = new ArrayAdapter<>(this,R.layout.list_item_nav_category);
         navCategoryAdapter.addAll("Upcoming", "Past Events", "Blog");
 
         navList.setAdapter(navCategoryAdapter);
@@ -76,6 +86,32 @@ public class MainActivity extends AppCompatActivity {
         navDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
 
+
+        navList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                navList.setItemChecked(position, true);
+                navDrawerLayout.closeDrawer(drawerFrame);
+                goToFragment(position);
+
+            }
+        });
+
+
+    }
+
+    private void goToFragment (int navItemPosition) {
+        Fragment newFragment;
+        switch (navItemPosition) {
+            default:
+                newFragment = EventListFragment.newInstance(1);
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, newFragment).commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
 
     }
 }
